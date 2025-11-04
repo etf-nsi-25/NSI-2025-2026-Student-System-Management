ba20f791b0afcb8e6b8362bd001f581fa15348a0
Griza20 <agrizovic1@etf.unsa.ba>
2025-11-04 22:54:09 +0100

    Remove bin and obj folders.

--- Application/Program.cs
+++ Application/Program.cs
@@ -50,11 +50,7 @@ app.UseAuthorization();
 
 // Enable Swagger in all environments
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UNSA SMS API V1");
-    c.RoutePrefix = string.Empty; // Swagger at root
-});
+app.UseSwaggerUI();
 
 // Map controllers
 app.MapControllers();
