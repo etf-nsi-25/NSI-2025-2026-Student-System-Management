9b2eca4d988929b6e9110fdf4cca7303276e737b
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2025-11-12 07:14:35 +0100

    added mappers, baserepository

---
+++ Modules/Identity/Identity.Infrastructure/Mappers/UserMapper.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Core.Entities;
+using Identity.Infrastructure.Entities;
+
+namespace Identity.Infrastructure.Mappers;
+
+public static class UserMapper
+{
+    public static User ToCore(this ApplicationUser appUser)
+    {
+        return new User(Email: appUser.Email, Id: appUser.Id);
+    }
+
+    public static ApplicationUser ToInfrastructure(this User user)
+    {
+        return new ApplicationUser
+        {
+            Id = user.Id,
+            UserName = user.Email,
+            Email = user.Email
+        };
+    }
+
+    public static void UpdateFrom(this ApplicationUser appUser, User user)
+    {
+        appUser.Email = user.Email;
+        appUser.UserName = user.Email;
+
+    }
+}
--- Modules/Identity/Identity.Infrastructure/Repositories/UserRepository.cs
+++ Modules/Identity/Identity.Infrastructure/Repositories/UserRepository.cs
@@ -2,19 +2,26 @@ using Identity.Core.Entities;
 using Identity.Core.Repositories;
 using Identity.Infrastructure.Db;
 using Identity.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace Identity.Infrastructure.Repositories;
 
-public class UserRepository(AuthDbContext context) : IUserRepository
+public class UserRepository : BaseRepository<ApplicationUser>, IUserRepository
 {
-    // TODO: think about creating BaseRepository abstract class as there will be a lot of repetetive code
-    // A guide can be found here: https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UserRepository(AuthDbContext context, UserManager<ApplicationUser> userManager)
+        : base(context)
+    {
+        _userManager = userManager;
+    }
+
     public async Task<User> CreateUser(string email)
     {
         ApplicationUser newUser = new ApplicationUser { UserName = email };
-        await context.Users.AddAsync(newUser);
+        await context.AddAsync(newUser);
 
-        // TODO: implement mappers
+ 
         return new User(newUser.Id, newUser.UserName);
     }
 
