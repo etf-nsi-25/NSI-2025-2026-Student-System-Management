8294753c77ecc858b7f304b6e4ae3c0929efb4cc
ihasicic1 <ihasicic1@etf.unsa.ba>
2025-12-03 00:58:38 +0100

    add university module tables

---
+++ Modules/University/University.Infrastructure/Entities/Faculty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Infrastructure.Entities
+{
+    public class Faculty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Address { get; set; }
+        public string Code { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime EstablishedDate { get; set; }
+        public string? DeanName { get; set; }
+
+        public ICollection<Department> Departments { get; set; } = new List<Department>();
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
