46cd858cf0b7032ca1238154d0aad6de1fd199ed
azunic3 <azunic3@etf.unsa.ba>
2025-12-04 00:58:54 +0100

    minor change

--- Modules/Faculty/Faculty.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Modules/Faculty/Faculty.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,13 +2,14 @@
 using Faculty.Application.Services;
 using Faculty.Core.Interfaces;
 using Faculty.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Faculty.Infrastructure.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddFacultyModule(this IServiceCollection services)
+        public static IServiceCollection AddFacultyModule(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<ICourseRepository, CourseRepository>();
             services.AddScoped<ICourseService, CourseService>();
