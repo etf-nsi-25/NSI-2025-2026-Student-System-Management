361a3c66ad05bb3b71e562858bcc92aefabfd5e7
apasalic10 <apasalic1@etf.unsa.ba>
2025-12-09 00:12:23 +0100

    Add migration

--- Modules/Support/Support.Application/DTOs/CreateDocumentRequestSTO.cs
+++ Modules/Support/Support.Application/DTOs/CreateDocumentRequestSTO.cs
@@ -13,7 +13,7 @@ namespace Support.Application.DTOs
 		public string UserId { get; set; }
 
 		[Range(1, int.MaxValue, ErrorMessage = "FacultyId must be greater than 0.")]
-		public int FacultyId { get; set; }
+		public Guid FacultyId { get; set; }
 
 		[Required(ErrorMessage = "DocumentType is required.")]
 		[StringLength(100, ErrorMessage = "DocumentType cannot exceed 100 characters.")]
