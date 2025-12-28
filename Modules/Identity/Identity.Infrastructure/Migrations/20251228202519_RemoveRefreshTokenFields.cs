9084d43168a3d8260c643a2af6f1d79b8c55fd71
amarderschrecklicher <atahirovic3@etf.unsa.ba>
2025-12-28 21:26:37 +0100

    migration added

---
+++ Modules/Identity/Identity.Infrastructure/Migrations/20251228202519_RemoveRefreshTokenFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRefreshTokenFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UserAgent",
+                table: "RefreshTokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserAgent",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
