d97973162205df1d16c638228587f0c7a253b923
Ahmed Ljubunčić <ahmedlj07@gmail.com>
2025-12-31 13:05:13 +0100

    PBI-568 Use InMemory Database for integration tests

--- Modules/Identity/Identity.IntegrationTests/IdentityApiFactory.cs
+++ Modules/Identity/Identity.IntegrationTests/IdentityApiFactory.cs
@@ -3,6 +3,7 @@ using Identity.API.Controllers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,15 @@ namespace Identity.IntegrationTests
 {
     public class IdentityApiFactory : WebApplicationFactory<AuthController>
     {
+        private readonly IServiceProvider _efServiceProvider;
+
+        public IdentityApiFactory()
+        {
+            _efServiceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureAppConfiguration((context, config) =>
@@ -24,6 +34,44 @@ namespace Identity.IntegrationTests
 
             builder.ConfigureServices(services =>
             {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<AuthDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                var contextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(AuthDbContext));
+                if (contextDescriptor != null)
+                {
+                    services.Remove(contextDescriptor);
+                }
+
+                var optionsDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions));
+                if (optionsDescriptor != null)
+                {
+                    services.Remove(optionsDescriptor);
+                }
+
+                services.AddDbContext<AuthDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("IdentityIntegrationTestDb");
+                    options.UseInternalServiceProvider(_efServiceProvider);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<AuthDbContext>();
+                    db.Database.EnsureCreated();
+                }
+
                 services.AddIdentity<ApplicationUser, IdentityRole>()
                     .AddEntityFrameworkStores<AuthDbContext>()
                     .AddDefaultTokenProviders();
